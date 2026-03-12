5f7468402e6519c82f4cdc14711de80683ede6ba
Rayan K <r.kinet@students.ephec.be>
2026-03-12 13:50:57 +0100

    ajout

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+// Définition d'une interface pour le typage
+// Sera couvert plus en profondeur en TH
+interface User {
+  id: number;
+  name: string;
+}
+function App() {
+  // 1. Définition de l'état
+  const [data, setData] = useState<User[]>([]);
+  // 2. Appel API au montage du composant
+  useEffect(() => {
+    fetch("http://localhost:3000/api/data")
+      .then(res => res.json())
+      .then(result => setData(result))
+      .catch(err => console.error(err));
+  }, []);
+  // 3. Rendu (JSX)
+  return (
+    <div>
+      <h1>Liste des utilisateurs</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.firstName}>{item.lastName}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
